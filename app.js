183e8e2c378b5fa1715e4b538260a3aa9c8ed421
Kraaackle <saliba.andrew@outlook.com>
2021-08-04 13:23:59 -0400

    populated name on search

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const selectTag = document.querySelector('#select-character')
 const form = document.querySelector('form')
-const dataContainer = document.querySelector('#data-container')
+
 
 
 const getAllCharacters = async () => {
   try {
     const allCharacterURL = `https://futuramaapi.herokuapp.com/api/v2/characters`
     const response = await axios.get(allCharacterURL)
-    // console.log(response.data)
-    
+    console.log(response.data)
+
 
     optionTags(response.data)
     return response
@@ -22,18 +22,19 @@ getAllCharacters()
 function optionTags(charList) {
   // console.log(charList)
 
-  charList.forEach((char) => {
+  charList.forEach((char, index) => {
     // console.log(char.Name)
+    // console.log(index)
     let optionTag = document.createElement('option')
     // console.log(optionTag)
     optionTag.textContent = char.Name
-    optionTag.value = char.Name
-    
-    document.querySelector("#select-character").append(optionTag)
- 
- 
+    optionTag.value = index
+
+    selectTag.append(optionTag)
+
+
   })
-  
+
 }
 
 console.log(form)
@@ -42,7 +43,42 @@ form.addEventListener("submit", (e) => {
   e.preventDefault()
   const charValue = document.querySelector('select').value
   console.log(charValue)
+  getData(charValue)
 })
 
+// ------------------------------------------------------------------
+
+renderList = (render) => {
+  const dataContainer = document.querySelector('#data-container')
+
+
+
+  let nameEl = document.createElement("h1")
+  dataContainer.appendChild(nameEl)
+  nameEl.textContent = `${render[selectTag.value].Name}`
+  nameEl.value = `${render[selectTag.value].Name}`
+
+  // let planetEl = document.createElement("h2")
+  // dataContainer.appendChild(planetEl)
+  // planetEl.textContent = `${data.Planet}`
+  // planetEl.value = `${info.Planet}`
+
+  // })
+}
+
+
+
+
+const getData = async (charValue) => {
+
+  try {
+    const data = await axios.get('http://futuramaapi.herokuapp.com/api/v2/characters?search=')
+    renderList(data.data)
+    return data
+
+  } catch (err) {
+    console.error(err)
+  }
+}
 
 
