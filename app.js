c676124fc10b222f7da4f04d7ebb2ca32678e919
Kraaackle <saliba.andrew@outlook.com>
2021-08-04 13:39:54 -0400

    finsihed MVP/API is now functioning

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ form.addEventListener("submit", (e) => {
 
 renderList = (render) => {
   const dataContainer = document.querySelector('#data-container')
-
+  removeElement(dataContainer)
 
 
   let nameEl = document.createElement("h1")
@@ -58,19 +58,35 @@ renderList = (render) => {
   nameEl.textContent = `${render[selectTag.value].Name}`
   nameEl.value = `${render[selectTag.value].Name}`
 
-  // let planetEl = document.createElement("h2")
-  // dataContainer.appendChild(planetEl)
-  // planetEl.textContent = `${data.Planet}`
-  // planetEl.value = `${info.Planet}`
+  let planetEl = document.createElement("h2")
+  dataContainer.appendChild(planetEl)
+  planetEl.textContent = `${render[selectTag.value].Planet}`
+  planetEl.value = `${render[selectTag.value].Planet}`
 
-  // })
+  let picPic = document.createElement('img')
+  dataContainer.appendChild(picPic)
+  picPic.src = `${render[selectTag.value].PicUrl}`
+
+  let speciesEl = document.createElement("h2")
+  dataContainer.appendChild(speciesEl)
+  speciesEl.textContent = `${render[selectTag.value].Species}`
+  speciesEl.value = `${render[selectTag.value].Species}`
+
+  let voiceEl = document.createElement("h2")
+  dataContainer.appendChild(voiceEl)
+  voiceEl.textContent = `${render[selectTag.value].VoicedBy}`
+  voiceEl.value = `${render[selectTag.value].VoicedBy}`
+
+  let professionEl = document.createElement("h3")
+  dataContainer.appendChild(professionEl)
+  professionEl.textContent = `${render[selectTag.value].Profession}`
+  professionEl.value = `${render[selectTag.value].Profession}`
 }
 
 
 
 
 const getData = async (charValue) => {
-
   try {
     const data = await axios.get('http://futuramaapi.herokuapp.com/api/v2/characters?search=')
     renderList(data.data)
@@ -78,6 +94,13 @@ const getData = async (charValue) => {
 
   } catch (err) {
     console.error(err)
+  }
+}
+
+
+function removeElement(element) {
+  while (element.lastChild) {
+    element.removeChild(element.lastChild)
   }
 }
 
